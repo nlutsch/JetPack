b32362e2dcb6647fbda8ac3f8db927898b82ac9b
Nick <lutschn1@central.edu>
2015-08-11 18:52:12 -0500

    Added F-16 Model and Flight Script

---
+++ JetPack/Assets/Flight.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flight : MonoBehaviour
+{
+
+
+    public float Speed = 100.0f;
+    public float Rotation = 200.0f;
+
+    Vector3 MoveDirection = Vector3.zero;
+    public Rigidbody Controller;
+    public GameObject Empty;
+
+    // Use this for initialization
+    void Start()
+    {
+        Controller = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        MoveDirection = new Vector3(MoveDirection.x, MoveDirection.y, 1);
+        MoveDirection *= Speed;
+        this.transform.Translate(MoveDirection * Time.deltaTime);
+        if (Empty.transform.rotation.z > 35) 
+        {
+            Empty.transform.rotation = Quaternion.Euler(Empty.transform.rotation.x, Empty.transform.rotation.y, 35); 
+        }
+    }
+}
---
+++ JetPack/Assets/LThruster.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LThruster : MonoBehaviour {
+
+    public float Force = 10.0f;
+    public Rigidbody LeftThruster;
+    private Vector3 Direction = new Vector3(.2f, 1, 0);
+
+	// Use this for initialization
+	void Start () 
+    {
+        LeftThruster = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        LeftThruster.AddForce(Direction * Force, ForceMode.Impulse);
+	}
+}
